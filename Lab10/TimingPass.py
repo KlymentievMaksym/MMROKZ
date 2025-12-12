d33c49c383029b9c55a423c3c0d622cdeb7db99b
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-12-12 18:02:30 +0200

    Lab 10 DOne?

--- Lab10/TimingPass.py
+++ Lab10/TimingPass.py
@@ -1,8 +1,9 @@
 import time
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, f
 from pynput import keyboard
 import sys
+import os
 
 class TimingPass:
     def __init__(self, target_phrase="mama"):
@@ -12,7 +13,7 @@ class TimingPass:
 
     def on_press(self, key):
         current_time = time.time()
-        
+
         if self.last_press == 0:
             self.last_press = current_time
             return
@@ -20,7 +21,7 @@ class TimingPass:
         if key == keyboard.Key.backspace:
             if self.timings:
                 self.timings.pop()
-            
+
             self.last_press = current_time
             return
 
@@ -37,65 +38,140 @@ class TimingPass:
 
         listener = keyboard.Listener(on_press=self.on_press)
         listener.start()
-        text = input(f"[{attempt_name}] Enter '{self.target_phrase}' and then press Enter:\n>>> ")
+        text = input(f"[{attempt_name}] Enter '{self.target_phrase}':\n>>> ")
         listener.stop()
-            
-        print(f"[Length] {len(self.timings)}")
+        if text != self.target_phrase:
+            print("[Access Denied] Wrong phrase")
+            return self.capture_attempt(attempt_name)
         return np.array(self.timings)
 
-    def train(self, iterations: int = 5):
+    def _load_content(self, file_path: str) -> np.array:
+        with open(file_path, "r") as f:
+            return np.genfromtxt(f, delimiter=",", dtype=float)
+
+    def filter_outliers(self, data):
+        filtered_data = data.copy()
+        if len(data) == 0: return data
+        mean = np.mean(data)
+        std = np.std(data)
+
+        lower_bound = mean - 3 * std
+        upper_bound = mean + 3 * std
+        for row in data:
+            if (row < lower_bound).any() or (row > upper_bound).any():
+                filtered_data = np.delete(filtered_data, np.where(filtered_data == row), axis=0)
+        # print(filtered_data)
+
+        return filtered_data
+
+    def train(self, iterations: int = 5, training_data_path: str = None, save: bool = False, read: bool = False, rewrite: bool = False):
         print("[Training]")
-        clean_profiles = []
-        
-        for i in range(iterations):
-            data = self.capture_attempt(f"{i+1}/{iterations}")
-
-            # Фільтрація грубих помилок (якщо довжина не збігається)
-            # В реальності тут має бути алгоритм з методички (відкидання викидів)
-            expected_intervals = len(self.target_phrase) - 1
-            if len(data) != expected_intervals:
-                print(f"[Error] Expected: {expected_intervals}, Received: {len(data)}")
-            else:
+        if read and os.path.exists(training_data_path) and os.path.isfile(training_data_path) and len(self._load_content(training_data_path)) > 0:
+            self.reference_data = self._load_content(training_data_path)
+        else:
+            clean_profiles = []
+            
+            for i in range(iterations):
+                data = self.capture_attempt(f"{i+1}/{iterations}")
                 clean_profiles.append(data)
-        
-        if not clean_profiles:
-            print("[Error] No data collected")
-            sys.exit()
 
-        # Об'єднуємо всі спроби в один масив для кожного інтервалу
-        # Для спрощення беремо середнє затримки по всіх спробах
-        self.reference_data = np.concatenate(clean_profiles)
-        
-        mean_val = np.mean(self.reference_data)
-        var_val = np.var(self.reference_data)
-        print(f"[Mean] {mean_val:.4f} | [Variance] {var_val:.4f}")
+            if not clean_profiles:
+                print("[Error] No data collected")
+                sys.exit()
 
-    def test(self, iterations: int = 5):
+            self.reference_data = np.array(clean_profiles)
+
+        # print(self.reference_data.shape)
+        self.reference_data = self.filter_outliers(self.reference_data)
+        # print(self.reference_data.shape)
+
+        if save:
+            if not rewrite:
+                self.reference_data = np.concatenate((self.reference_data, self.filter_outliers(self._load_content(training_data_path))))
+
+            np.savetxt(training_data_path, self.reference_data, delimiter=",")
+
+    def fisher_test(self, data1, data2):
+        var1 = np.var(data1, ddof=1)
+        var2 = np.var(data2, ddof=1)
+        
+        # F = S_max^2 / S_min^2
+        if var1 > var2:
+            f_stat = var1 / var2
+            df1, df2 = len(data1) - 1, len(data2) - 1
+        else:
+            f_stat = var2 / var1
+            df1, df2 = len(data2) - 1, len(data1) - 1
+            
+        p_value = 1 - f.cdf(f_stat, df1, df2)
+        return p_value * 2
+
+    def test(self, alpha: float = 0.05):
         print(f"[Testing]")
-        for iteration in range(iterations):
-            sample = self.capture_attempt("Test")
-            
-            if len(sample) == 0:
-                print("Empty sample")
-                return
+        samples = self.capture_attempt("Test")
+        if len(samples) == 0:
+            print("[Error] No data")
+            return
+        samples = np.array(samples)
 
-            t_stat, p_value = ttest_ind(self.reference_data, sample, equal_var=False)
-            
-            print("-" * 45)
-            print(f"T: {t_stat:.4f}")
-            print(f"P-value:   {p_value:.4f}")
-            print("-" * 45)
+        ref_flat = self.reference_data.flatten()
+        p_value_f = self.fisher_test(ref_flat, samples)
+        variances_equal = p_value_f > alpha
 
-            alpha = 0.05
+        t_stat, p_value = ttest_ind(ref_flat, samples, equal_var=variances_equal)
 
-            res = p_value > alpha
-            if res:
-                print("[Access Granted]")
-            else:
-                print("[Access Denied]")
-            
+        print("-" * 45)
+        print(f"[Fisher P-value] {p_value_f:.4f} {"EQUAL" if variances_equal else "NOT EQUAL"}")
+        print(f"[T-test P-value] {p_value:.4f}")
+        print("-" * 45)
+
+        res = p_value > alpha
+        if res:
+            print("[Access Granted]")
+        else:
+            print("[Access Denied]")
+        return res
+
+    def calculate_errors(self, tests_amount: int = 100, alpha: float = 0.05):
+        print(f"[Calculating errors]")
+        
+        if len(self.reference_data) == 0:
+            print("[Error] Спочатку пройдіть навчання!")
+            return
+
+        ref_mean = np.mean(self.reference_data)
+        ref_std = np.std(self.reference_data)
+
+        imposter_data = self.reference_data * 2
+        imp_mean = np.mean(imposter_data)
+        imp_std = np.std(imposter_data)
+
+        sample_size = self.reference_data.shape[1]
+        false_rejections = 0
+        for _ in range(tests_amount):
+            simulated_attempt = np.random.normal(ref_mean, ref_std, sample_size)
+            t_stat, p_val = ttest_ind(self.reference_data.flatten(), simulated_attempt, equal_var=False)
+            if p_val <= alpha:
+                false_rejections += 1
+
+        p1 = false_rejections / tests_amount
+        print(f"[Score] P1: {p1:.2f} ({p1*100:.2f}%)")
+
+        false_acceptances = 0
+        for _ in range(tests_amount):
+            simulated_impostor = np.random.normal(imp_mean, imp_std, sample_size)
+            t_stat, p_val = ttest_ind(self.reference_data.flatten(), simulated_impostor, equal_var=False)
+            if p_val > alpha:
+                false_acceptances += 1
+
+        p2 = false_acceptances / tests_amount
+        print(f"[Score] P2: {p2:.2f} ({p2*100:.2f}%)")
+
 
 if __name__ == "__main__":
-    app = TimingPass("dlagnytor") 
-    app.train(iterations=5)
-    app.test(iterations=3)
+    app = TimingPass("dlagnytor")
+    # app = TimingPass("mama")
+    app.train(iterations=20, training_data_path="./Lab10/Data/training_data.txt", save=False, read=True, rewrite=True)
+    # while True:
+    app.test(alpha=.1)
+    app.calculate_errors(tests_amount=1000, alpha=.1)
